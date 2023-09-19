42be08c788fcab98d8168733d9dda53c48c335c6
SideralShift <eduardomendoza@Eduardos-MacBook-Pro.local>
2023-09-18 20:33:35 -0600

    Added support to close photo viewer with drag gesture

--- lib/widgets/reusable/MiniGallery/mini_gallery.dart
+++ lib/widgets/reusable/MiniGallery/mini_gallery.dart
@@ -1,13 +1,14 @@
 import 'package:app/models/attachment.dart';
 import 'package:app/utils/classes/storage_image.dart';
 import 'package:app/widgets/reusable/MiniGallery/mini_gallery_image.dart';
+import 'package:app/widgets/reusable/MiniGallery/photo_viewer_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
 class MiniGallery extends StatelessWidget {
-  final List<Image?> images;
+  List<Image?> images = [];
 
   MiniGallery({required this.images});
 
@@ -19,81 +20,7 @@ class MiniGallery extends StatelessWidget {
       barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
       pageBuilder: (BuildContext buildContext, Animation animation,
           Animation secondaryAnimation) {
-        return Scaffold(
-          body: Stack(
-            children: [
-              // Image with fitted box
-              PhotoViewGallery(
-                pageOptions: images
-                    .map((e) =>
-                        PhotoViewGalleryPageOptions(imageProvider: e?.image))
-                    .toList(),
-              ),
-
-              // Gradient overlay at the top
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                height: 150, // Fixed height
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.black.withOpacity(0.7),
-                        Colors.transparent,
-                        // Adjust the opacity and color as needed
-                      ],
-                      stops: [
-                        0.0,
-                        0.7
-                      ], // Adjust the stops to control the gradient distribution
-                    ),
-                  ),
-                ),
-              ),
-
-              // Gradient overlay at the bottom
-              Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  height: 150, // Fixed height
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.center,
-                        colors: [Colors.black, Colors.transparent],
-                      ),
-                    ),
-                  )),
-
-              // Circular button at the top left
-              Positioned(
-                top: 45,
-                left: 10,
-                child: SizedBox(
-                  height: 25,
-                  child: FloatingActionButton(
-                    mini: true,
-                    elevation: 0,
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      SystemChrome.setEnabledSystemUIMode(
-                          SystemUiMode.edgeToEdge);
-                    },
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                    child: Icon(Icons.close),
-                  ),
-                ),
-              )
-            ],
-          ),
-        );
+        return PhotoViewerScreen(images: images);
       },
     );
   }
---
+++ lib/widgets/reusable/MiniGallery/photo_viewer_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+
+class PhotoViewerScreen extends StatelessWidget {
+  final List<Image?> images;
+
+  PhotoViewerScreen({required this.images});
+
+  _closePhotoViewer(BuildContext context) {
+    Navigator.of(context).pop();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Dismissible(
+      resizeDuration: null,
+      key: UniqueKey(),
+      direction: DismissDirection.vertical,
+      onDismissed: (_) {
+        // Close the photo viewer when dragged down
+        _closePhotoViewer(context);
+      },
+      child: Scaffold(
+          body: Stack(
+        children: [
+          // Image with fitted box
+          PhotoViewGallery(
+            pageOptions: images
+                .map(
+                    (e) => PhotoViewGalleryPageOptions(imageProvider: e?.image))
+                .toList(),
+          ),
+
+          // Gradient overlay at the top
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0, // Fixed height
+            child: Container(
+              height: 120,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.black.withOpacity(0.7),
+                    Colors.transparent,
+                    // Adjust the opacity and color as needed
+                  ],
+                  stops: [
+                    0.0,
+                    0.7
+                  ], // Adjust the stops to control the gradient distribution
+                ),
+              ),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: SizedBox(
+                      height: 25,
+                      child: FloatingActionButton(
+                        mini: true,
+                        elevation: 0,
+                        onPressed: () {
+                          _closePhotoViewer(context);
+                        },
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        child: Icon(Icons.close),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          // Gradient overlay at the bottom
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              height: 150, // Fixed height
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.center,
+                    colors: [Colors.black, Colors.transparent],
+                  ),
+                ),
+              )),
+        ],
+      )),
+    );
+  }
+}
